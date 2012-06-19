75aa20ae8780409dc7ec8a110cc1df2347b5eb79
Andrew Crichton <andrew.g.crichton@gmail.com>
2012-06-19 02:24:51 -0400

    Some install scripts for windows and linux with a windows binary packaged.

---
+++ install/install.bat
@@ -0,0 +1,7 @@
+adb uninstall com.professionalperformance.geotracker
+
+adb install GeoTracker.apk
+
+adb shell am start -n com.professionalperformance.geotracker/.EnforceGPSActivity
+
+adb shell am broadcast -a android.intent.action.BOOT_COMPLETED
